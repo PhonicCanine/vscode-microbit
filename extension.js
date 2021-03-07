b9e12e98e4338ae4ef18a54dae763ff5e6d640d6
Joseph-Fergusson-CGS <66996117+Joseph-Fergusson-CGS@users.noreply.github.com>
2021-03-08 03:13:33 +1100

    Updated to support Python3 -m as a fallback, and added ufs options

--- extension.js
+++ extension.js
@@ -1,4 +1,6 @@
 const vscode = require('vscode');
+const child_process = require('child_process');
+const rimraf = require("rimraf");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,39 +10,60 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	console.log('Congratulations, your extension "micro-bit" is now active!');
-
+	let output = vscode.window.createOutputChannel("MicroBuild");
+	let defaultFallbackBuild = false;
 	function buildCurrentDocument(){
-
-		var t;
-		var defined = false;
-		var terminalName = "MicroBuild";
-		if (vscode.window.terminals.length != 0){
-			for (var i = 0; i < vscode.window.terminals.length; i++){
-				if (vscode.window.terminals[i].name == terminalName){
-					t = vscode.window.terminals[i];
-					defined = true;
-					break;
-				}
-			}
-			if (t === undefined && defined == false){
-				t = vscode.window.createTerminal(terminalName);
-			}
-		} else {
-			t = vscode.window.createTerminal(terminalName);
-		}
 
 		console.log(vscode.window.activeTextEditor.document.fileName);
 
-		var text = "uflash \"" + vscode.window.activeTextEditor.document.fileName + "\"";
-		console.log(text);
+		function python3Func() {
+			child_process.exec("python3 -m uflash \"" + vscode.window.activeTextEditor.document.fileName + "\"", (error, stdout, stderr) => {
+			
+				if (error) {
+					console.log();
+					output.appendLine(`error: ${error.message}.`);
+					return;
+				}
+				if (stderr) {
+					output.appendLine(`error: ${error.message}.`);
+					return;
+				}
+				if (!(error || stderr)) {
+					output.appendLine(stdout);
+					output.appendLine("Successfully built to Microbit");
+					defaultFallbackBuild = true;
+				}
+			});
+		}
 
-		t.sendText(text);
-
+		if (!defaultFallbackBuild) {
+			child_process.exec("uflash \"" + vscode.window.activeTextEditor.document.fileName + "\"", (error, stdout, stderr) => {
+				
+				if (error) {
+					console.log();
+					output.appendLine(`error: ${error.message}. Trying again with Python3.`);
+					python3Func();
+					return;
+				}
+				if (stderr) {
+					output.appendLine(`error: ${error.message}. Trying again with Python3.`);
+					python3Func();
+					return;
+				}
+				if (!(error || stderr)) {
+					output.appendLine(stdout);
+					output.appendLine("Successfully built to Microbit");
+				}
+			});
+		} else {
+			python3Func();
+		}
+		
 	}
 
 	let buildCommand = vscode.commands.registerCommand('extension.build', function () {
 		buildCurrentDocument();
+		output.show(true);
 	});
 
 	function fetchMicrobitModule(){
@@ -68,11 +91,17 @@ function activate(context) {
 		
 		if (openFolders.length == 0){
 			vscode.window.showErrorMessage("Please open a folder before importing the micro:bit module")
-		}else{
+		} else {
 			
-			t.sendText("rm -rf microbit")
-			t.sendText("rd /s /q microbit")
-			t.sendText("git clone https://github.com/PhonicCanine/microbit.git")
+			rimraf("microbit", (err) => {
+				if (err) {
+					output.appendLine(`Error -- Could not delete existing folder ${err}`);
+				} else {
+					output.appendLine("Deleted existing microbit folder");
+					t.sendText("git clone https://github.com/PhonicCanine/microbit.git");
+				}
+			});
+			
 			
 		}
 
@@ -80,10 +109,166 @@ function activate(context) {
 
 	let getCommand = vscode.commands.registerCommand('extension.get', function(){
 		fetchMicrobitModule();
-	})
+	});
+
+	function ufsPutCommand(){
+		console.log(vscode.window.activeTextEditor.document.fileName);
+
+		function python3Func() {
+			child_process.exec("python3 -m ufs put \"" + vscode.window.activeTextEditor.document.fileName + "\"", (error, stdout, stderr) => {
+			
+				if (error) {
+					console.log();
+					output.appendLine(`error: ${error.message}.`);
+					return;
+				}
+				if (stderr) {
+					output.appendLine(`error: ${error.message}.`);
+					return;
+				}
+				if (!(error || stderr)) {
+					output.appendLine(stdout);
+					output.appendLine("Successfully copied to Microbit");
+					defaultFallbackBuild = true;
+				}
+			});
+		}
+
+		if (!defaultFallbackBuild) {
+			child_process.exec("ufs put \"" + vscode.window.activeTextEditor.document.fileName + "\"", (error, stdout, stderr) => {
+				
+				if (error) {
+					console.log();
+					output.appendLine(`error: ${error.message}. Trying again with Python3.`);
+					python3Func();
+					return;
+				}
+				if (stderr) {
+					output.appendLine(`error: ${error.message}. Trying again with Python3.`);
+					python3Func();
+					return;
+				}
+				if (!(error || stderr)) {
+					output.appendLine(stdout);
+					output.appendLine("Successfully copied to Microbit");
+				}
+			});
+		} else {
+			python3Func();
+		}
+	}
+
+	let putCommand = vscode.commands.registerCommand('extension.put', function(){
+		ufsPutCommand();
+		output.show(true);
+	});
+
+	function ufsRm(filename){
+		if (filename == "")
+			return;
+
+		function python3Func() {
+			child_process.exec("python3 -m ufs rm " + filename, (error, stdout, stderr) => {
+			
+				if (error) {
+					console.log();
+					output.appendLine(`error: ${error.message}.`);
+					return;
+				}
+				if (stderr) {
+					output.appendLine(`error: ${error.message}.`);
+					return;
+				}
+				if (!(error || stderr)) {
+					defaultFallbackBuild = true;
+					output.appendLine(`Deleted ${filename}`);
+				}
+			});
+		}
+
+		if (!defaultFallbackBuild) {
+			child_process.exec("ufs rm " + filename, (error, stdout, stderr) => {
+				
+				if (error) {
+					console.log();
+					output.appendLine(`error: ${error.message}. Trying again with Python3.`);
+					python3Func();
+					return;
+				}
+				if (stderr) {
+					output.appendLine(`error: ${error.message}. Trying again with Python3.`);
+					python3Func();
+					return;
+				}
+				if (!(error || stderr)) {
+					output.appendLine(`Deleted ${filename}`);
+				}
+			});
+		} else {
+			python3Func();
+		}
+	}
+
+	function ufsRemoveFiles(files) {
+		let filesList = files.split("\n");
+		for (let i = 0; i < filesList.length; i++) {
+			let file = filesList[i].trim();
+			ufsRm(file);
+		}
+	}
+
+	function ufsClear(){
+		function python3Func() {
+			child_process.exec("python3 -m ufs ls", (error, stdout, stderr) => {
+			
+				if (error) {
+					console.log();
+					output.appendLine(`error: ${error.message}.`);
+					return;
+				}
+				if (stderr) {
+					output.appendLine(`error: ${error.message}.`);
+					return;
+				}
+				if (!(error || stderr)) {
+					defaultFallbackBuild = true;
+					ufsRemoveFiles(stdout);
+				}
+			});
+		}
+
+		if (!defaultFallbackBuild) {
+			child_process.exec("ufs ls", (error, stdout, stderr) => {
+				
+				if (error) {
+					console.log();
+					output.appendLine(`error: ${error.message}. Trying again with Python3.`);
+					python3Func();
+					return;
+				}
+				if (stderr) {
+					output.appendLine(`error: ${error.message}. Trying again with Python3.`);
+					python3Func();
+					return;
+				}
+				if (!(error || stderr)) {
+					ufsRemoveFiles(stdout);
+				}
+			});
+		} else {
+			python3Func();
+		}
+	}
+
+	let clearCommand = vscode.commands.registerCommand('extension.clear', function(){
+		ufsClear();
+		output.show(true);
+	});
 
 	context.subscriptions.push(buildCommand);
 	context.subscriptions.push(getCommand);
+	context.subscriptions.push(putCommand);
+	context.subscriptions.push(clearCommand);
 }
 exports.activate = activate;
 
